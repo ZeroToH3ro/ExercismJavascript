ded1ea4a6920c404d7863e73cee0fb9d84abbe98
ZeroToH3ro <mtblaser2002@gmail.com>
2023-07-10 14:01:02 +0700

    add from zero-dev

--- javascript/scale-generator/scale-generator.js
+++ javascript/scale-generator/scale-generator.js
@@ -1,18 +1,74 @@
-//
-// This is only a SKELETON file for the 'Scale Generator' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const SHARPS = [
+  "A",
+  "A#",
+  "B",
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+];
+
+const FLATS = ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"];
+
+const INTERVALS = new Map([
+  ["m", 1],
+  ["M", 2],
+  ["A", 3],
+]);
+
+const TONE = /^[a-gA-G][#b]?$/;
+
+const useSharps = new Set([
+  "C",
+  "G",
+  "D",
+  "A",
+  "E",
+  "B",
+  "F#",
+
+  "a",
+  "e",
+  "b",
+  "f#",
+  "c#",
+  "g#",
+  "d#",
+]);
 
 export class Scale {
   constructor(tonic) {
-    throw new Error('Remove this statement and implement this function');
+    this.tonic = tonic || "C";
+  }
+
+  get tone() {
+    return this.tonic[0].toUpperCase() + this.tonic.slice(1);
   }
 
   chromatic() {
-    throw new Error('Remove this statement and implement this function');
+    const chrd = useSharps.has(this.tonic) ? SHARPS : FLATS;
+
+    const i = chrd.indexOf(this.tone);
+
+    return [...chrd.slice(i), ...chrd.slice(0, i)];
   }
 
   interval(intervals) {
-    throw new Error('Remove this statement and implement this function');
+    const chrd = this.chromatic();
+
+    return [...intervals]
+
+      .map((interval) => INTERVALS.get(interval))
+
+      .reduce((acc, i) => [...acc, acc[acc.length - 1] + i], [0])
+
+      .map((i) => chrd[i])
+
+      .filter((tone) => TONE.test(tone));
   }
 }
